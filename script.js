4f53b886a01e0b9ded493e8e5a388a3e849c5f2d
LAQNT <laura.quintero.a@hotmail.com>
2023-07-20 18:47:01 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function getAge(dateString) {
+  var today = new Date();
+  var birthDate = new Date(dateString);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+}
+
+console.log(getAge("1993-07-31"));
+
+function insertAge() {
+  const span = document.createElement("SPAN");
+  span.innerText = getAge("1993-07-31") + " years old";
+  document.querySelector(".info").appendChild(span);
+}
+
+insertAge();
